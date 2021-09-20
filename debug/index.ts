c008a1ead45d4b8471aaf6f2b5c0deecb5c1338f
Timur Olur <timurolur@gmail.com>
2021-09-20 18:48:00 +0300

    debugger CLI message and input improvements

--- debug/index.ts
+++ debug/index.ts
@@ -19,42 +19,48 @@ const eventLoop = () =>
 InitEnvironment()
     .then(rootElement =>
     {
-
         if (!rootElement)
         {
             throw new Error("Initialization failed. Root object is not valid.");
         }
 
-        // eslint-disable-next-line no-constant-condition
-        rl.on('line', (input) =>
+        console.log("Debugger started. see /help for options.");
+
+        rl.on('line', (rawInput) =>
         {
+            const input = rawInput.trim();
+
             if (input.toUpperCase() === "/EXIT")
             {
+                console.log("Debugger closing...".green);
                 process.exit(0);
             }
             else if (input.toUpperCase() === "/HELP")
             {
-                console.log("/EXIT \t\t- Exit from debugging.")
-                console.log("/HELP \t\t- Show help menu.")
-                console.log("/LIST \t\t- List mountable components")
-                console.log("/MNT [Name]  \t\t- Mount component. Automatically unmounts if a component is already mounted")
-                console.log("/UMNT \t\t- Unmount currently loaded component.")
-                console.log("/EVENT \t\t- Trigger a global event.")
+                console.log("");
+                console.log("/EXIT".padEnd(20, " "), "- Exit from debugging.");
+                console.log("/HELP".padEnd(20, " "), "- Show help menu.");
+                console.log("/LIST".padEnd(20, " "), "- List mountable components");
+                console.log("/MNT [Name]".padEnd(20, " "), "- Mount component. Automatically unmounts if a component is already mounted.");
+                console.log("/UMNT".padEnd(20, " "), "- Unmount currently loaded component.");
+                console.log("/EVENT".padEnd(20, " "), "- Trigger a global event.");
+                console.log("");
             }
             else if (input.toUpperCase() === "/LIST")
             {
-                const components = Reflect.ownKeys(Components);
+                const components = Object.keys(Components);
 
-                components.forEach(e => console.log(e.toString().yellow))
+                components.forEach(e => console.log(` * ${e.toString()}`.green))
             }
             else if (input.toUpperCase().startsWith("/MNT"))
             {
-                const options = input.split(" ");
+                const options = input.trim().split(" ");
 
                 if (options.length === 2 && Reflect.has(Components, options[1]))
                 {
                     ReactDOM.unmountComponentAtNode(rootElement);
                     ReactDOM.render(React.createElement(Components[options[1]]), rootElement);
+
                     console.log(`Mounted ${options[1]} to the root. You can dispatch events with /EVENT <eventName> .`.green);
                 }
                 else
@@ -67,18 +73,22 @@ InitEnvironment()
                 const unmount = ReactDOM.unmountComponentAtNode(rootElement);
                 console.log(unmount ? "Component unmounted".green : "There is no component to unmount".yellow);
             }
-            else if (input.toUpperCase() === "/EVENT")
+            else if (input.toUpperCase().startsWith("/EVENT"))
             {
-                const options = input.split(" ");
+                const options = input.trim().split(" ");
 
                 if (options.length === 2)
                 {
-                    window.dispatchEvent(new window.Event(input));
+                    window.dispatchEvent(new window.Event(options[1]));
                 }
                 else
                 {
                     console.log("Unexpected event input.".yellow);
                 }
+            }
+            else
+            {
+                console.log("Unknown command. See /help for more details.".yellow);
             }
         });
 
